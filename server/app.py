db124cab4c505c932b433088db79a0cdfb8645e5
kb674 <kushalbhikhabhai@gmail.com>
2021-05-25 20:44:35 +0000

    App now has a working mysql container. docker-compose deploy script runs compose up and eec to create tables.

--- server/app.py
+++ server/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 import requests
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
 
 class Animals(db.Model):
     id = db.Column(db.Integer, primary_key = True)
